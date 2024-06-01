1c7cbdafa5a41cf48169a2777af200cf1ae2323b
josephboamah <jfboamah@st.ug.edu.gh>
2024-06-01 16:24:04 -0700

    Code 1

---
+++ components/CardItem.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const CardItem = ({ title, description }) => {
+  return (
+    <View style={styles.card}>
+     
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+       
+      </View>
+      <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: 200,
+    backgroundColor: '#fff',
+    margin: 10,
+    borderRadius: 10, 
+    borderColor:'#E8D1BA',
+    overflow: 'hidden',
+    height: 230,
+  },
+  image: {
+    width: '100%',
+    height: 100,
+  },
+  textContainer: {
+    padding: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  description: {
+    color: '#9E9E9E',
+  },
+  readMore: {
+    color: '#B08D57',
+    marginTop: 5,
+  },
+});
+
+export default CardItem;
